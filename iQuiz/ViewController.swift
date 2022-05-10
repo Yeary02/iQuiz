d5fab5aa72842ad2a1b5b3967c4d89ff662a6e41
Yeary02 <yyuan02@uw.edu>
2022-05-09 17:12:31 -0700

    Initial Commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by 袁月 on 2022/5/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
